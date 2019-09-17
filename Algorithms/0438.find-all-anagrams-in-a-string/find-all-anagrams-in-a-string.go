847c4c0341035735f0008bf6f71349fd0201df9d
wuhaoxuan <haoxuan.wu@transsnet.com>
2019-09-17 15:12:13 +0800

    438.滑动窗口+词频统计

--- Algorithms/0438.find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.go
+++ Algorithms/0438.find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.go
@@ -1,28 +1,53 @@
 package problem0438
 
 func findAnagrams(s string, p string) []int {
-	var res = []int{}
-
+	//var res = []int{}
+	//
+	//var target, window [26]int
+	//for i := 0; i < len(p); i++ {
+	//	target[p[i]-'a']++
+	//}
+	//
+	//check := func(i int) {
+	//	if window == target {
+	//		res = append(res, i)
+	//	}
+	//}
+	//
+	//for i := 0; i < len(s); i++ {
+	//	window[s[i]-'a']++
+	//	if i == len(p)-1 {
+	//		check(0)
+	//	} else if len(p) <= i {
+	//		window[s[i-len(p)]-'a']--
+	//		check(i - len(p) + 1)
+	//	}
+	//}
+	//
+	//return res
+	var res []int
 	var target, window [26]int
+
 	for i := 0; i < len(p); i++ {
 		target[p[i]-'a']++
 	}
 
-	check := func(i int) {
-		if window == target {
-			res = append(res, i)
+	left, right := 0, 0
+	//当窗口右边出界 则退出
+	for right < len(s) {
+		//获取当前最右边的字符的hash值
+		idx := s[right] - 'a'
+		window[idx]++
+		right++
+		//当前字符的hash值 大于 目标数组（溢出） 1.当前字符不在目标数组中 2.当前字符有重复数组 左边界右移缩小窗口
+		for target[idx] < window[idx] {
+			window[s[left]-'a']--
+			left++
+		}
+		//当出现窗口大小等于目标数组的大小的时候 匹配成功+1
+		if right-left == len(p) {
+			res = append(res, left)
 		}
 	}
-
-	for i := 0; i < len(s); i++ {
-		window[s[i]-'a']++
-		if i == len(p)-1 {
-			check(0)
-		} else if len(p) <= i {
-			window[s[i-len(p)]-'a']--
-			check(i - len(p) + 1)
-		}
-	}
-
 	return res
 }
--- Algorithms/quick-sort/quick_sort.go
+++ Algorithms/quick-sort/quick_sort.go
@@ -14,18 +14,67 @@ func quick_sort(s []int, left, right int) {
 }
 
 func getPar(s []int, left, right int) int {
+	fast := left
+	last := right
 
+	leftLen := 0
+	rightLen := 0
+	l1 := left
+	r2 := right
 	tmp := s[left]
 	for left < right {
 		for right > left && s[right] >= tmp {
+			if s[right] == tmp {
+				s[right], s[r2] = s[r2], s[right]
+				rightLen++
+				r2--
+			}
 			right--
 		}
 		s[left] = s[right]
 		for right > left && s[left] <= tmp {
+			if s[left] == tmp {
+				s[l1], s[left] = s[left], s[l1]
+				leftLen++
+				l1++
+			}
 			left++
 		}
 		s[right] = s[left]
 	}
 	s[left] = tmp
+
+	i := left - 1
+	j := fast
+	for j < l1 && s[i] != tmp {
+		s[j], s[i] = s[i], s[j]
+		j++
+		i--
+	}
+
+	i2 := left + 1
+	j2 := last
+
+	for j2 > r2 && s[i2] != tmp {
+		s[i2], s[j2] = s[j2], s[i2]
+		i2++
+		j2--
+	}
+
 	return left
+}
+
+func setPartion(s []int, l, r int) {
+	mid := (l + r) / 2
+
+	if s[mid] > s[r] {
+		s[mid], s[r] = s[r], s[mid]
+	}
+	if s[l] > s[mid] {
+		s[l], s[mid] = s[mid], s[l]
+	}
+
+	if s[mid] < s[r] {
+		s[mid], s[l] = s[l], s[mid]
+	}
 }
