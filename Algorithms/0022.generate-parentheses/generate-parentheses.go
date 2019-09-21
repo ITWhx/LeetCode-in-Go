a2c440b3ef364487d3c334ac291afbd57fa2317c
wuhaoxuan <haoxuan.wu@transsnet.com>
2019-09-21 10:47:52 +0800

    22.括号生成（回溯法）

--- Algorithms/0022.generate-parentheses/generate-parentheses.go
+++ Algorithms/0022.generate-parentheses/generate-parentheses.go
@@ -29,3 +29,26 @@ func dfs(left, right, idx int, bytes []byte, res *[]string) {
 		dfs(left, right-1, idx+1, bytes, res)
 	}
 }
+
+func generateParenthesis2(n int) []string {
+	res := make([]string, n*n)
+	item := ""
+	generate(item, n, n, &res)
+	return res
+}
+
+func generate(item string, left, right int, res *[]string) {
+	//左括号和右括号满足上述条件的前提下都为n个，添加这个答案
+	if left == 0 && right == 0 {
+		*res = append(*res, item)
+		return
+	}
+	//左括号的个数小于n 才能继续放左括号
+	if left > 0 {
+		generate(item+"(", left-1, right, res)
+	}
+	//左括号个数必须大于右括号的放置个数 才能继续放右括号
+	if left < right {
+		generate(item+")", left, right+1, res)
+	}
+}
